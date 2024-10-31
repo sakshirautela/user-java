17a08eaaa68b9beddc48bdff522e4ec4ff97b75b
sakshirautela <techsakshirautela@gmail.com>
2024-10-31 18:01:20 +0530

    update

--- IndexesofSubArraySum.java
+++ IndexesofSubArraySum.java
@@ -4,6 +4,7 @@ public class IndexesofSubArraySum {
     static ArrayList<Integer> subarraySum(int[] arr, int target) {
         // code here
         ArrayList<Integer> al=new ArrayList<Integer>();
+        
     }
     public static void main(String[] args){
         System.out.println(subarraySum(new int[]{}, 0));
