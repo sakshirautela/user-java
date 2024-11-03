1e49b402824c03ae2a7b862e804fd44b2e51935f
sakshirautela <techsakshirautela@gmail.com>
2024-11-03 21:29:21 +0530

    Nov 2and 3 commits

---
+++ RotateString.java
@@ -0,0 +1,11 @@
+class Solution {
+
+    public static boolean rotateString(String s, String goal) {
+            if (s.length() != goal.length()) return false;
+            String doubledString = s + s;
+            return doubledString.contains(goal);
+        }
+        public static void main(String args[]){
+            System.out.println(rotateString("",""));
+    }
+}
