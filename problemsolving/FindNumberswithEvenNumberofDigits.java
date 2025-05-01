db21f754f1590d6fae6ae2280de8c00f6c6628b3
sakshirautela <techsakshirautela@gmail.com>
2025-05-01 23:32:58 +0530

    one april addition

--- problemsolving/FindNumberswithEvenNumberofDigits.java
+++ problemsolving/FindNumberswithEvenNumberofDigits.java
@@ -8,13 +8,22 @@ public class FindNumberswithEvenNumberofDigits {
         for (int i : nums) {
             int a = 0;
             while (i > 0) {
-                    a++;
+                a++;
                 i = i / 10;
             }
-            if(a%2==0){
+            if (a % 2 == 0) {
                 n++;
+            }
         }
-    }
         return n;
+    }
+
+    public int findNumbers2(int[] nums) {
+        int result = 0;
+        for (int number : nums) {
+            if ((number > 9 && number < 100) || (number > 999 && number < 10000) || (number == 100000))
+                result++;
+        }
+        return result;
     }
 }
