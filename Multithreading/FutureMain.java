afaad9db42fa30853d066bb8014163f7f8cbce1e
sakshirautela <techsakshirautela@gmail.com>
2025-05-01 23:35:23 +0530

    one april addition

---
+++ Multithreading/FutureMain.java
@@ -0,0 +1,17 @@
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class FutureMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<?> future = executorService.submit(() -> System.out.println("Hello")); // runnable parameter
+        System.out.println(future.get()); // blocking call ( null )
+        if(future.isDone()){
+            System.out.println("Task is done !");
+        }
+        executorService.shutdown();
+    }
+}
---
+++ Multithreading/ReentrantExample.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReentrantExample {
+    private final ReentrantLock lock = new ReentrantLock();
+
+    public void outerMethod() {
+        lock.lock();
+        try {
+            System.out.println("Outer method");
+            innerMethod();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void innerMethod() {
+        lock.lock();
+        try {
+            System.out.println("Inner method");
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+        ReentrantExample example = new ReentrantExample();
+        example.outerMethod();
+    }
+}
