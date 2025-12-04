7a1ebe07ac8b7653be6ce913f65802169741ae71
sakshirautela <techsakshirautela@gmail.com>
2025-12-04 15:15:16 +0530

    updates of daily challanges on leetcode collision count

---
+++ problemsolvingII/PathWithMinimumEffort.java
@@ -0,0 +1,9 @@
+public class PathWithMinimumEffort {
+    public static void main(String[] args) {
+        System.out.println(minimumEffortPath(new int[][]{}));
+    }
+
+    public static int minimumEffortPath(int[][] heights) {
+        return 0;
+    }
+}
---
+++ problemsolvingII/TheKnightsTourProblem.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class TheKnightsTourProblem {
+    public static void main(String[] args) {
+    }
+
+    public ArrayList<ArrayList<Integer>> knightTour(int n) {
+        // code here
+        return new ArrayList<>();
+    }
+}
